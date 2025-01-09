dbe5dcc252673f2496f6e968f1f9c818377db011
Dawid-zsp3 <uT_3772@zsp3zamosc.pl>
2025-01-09 09:12:24 +0100

    Dodaj pliki projektów.

---
+++ blad3/Program.cs
@@ -0,0 +1,16 @@
+﻿public class LogicalOperators
+{
+    public static void Main(string[] args)
+    {
+        int x = 10;
+        int y = 20;
+        if (x > 5 || y < 15 && x < 30)
+        {
+            Console.WriteLine("Warunek spelniony ");
+        }
+        else
+        {
+            Console.WriteLine("Warunek niespelniony");
+        }
+    }
+}
